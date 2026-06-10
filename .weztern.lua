99892742142ac77026383a3350e2863a9b978006
igorgbr <igorgphotoarte@gmail.com>
2026-06-09 21:17:17 -0300

    ola alquimista

---
+++ .weztern.lua
@@ -0,0 +1,8 @@
+local wezterm = require 'wezterm'
+
+return {
+  color_scheme = 'Red Alert (Gogh)',
+  font_size = 14,
+  enable_tab_bar = true,           
+  window_background_opacity = 0.44,   
+}
